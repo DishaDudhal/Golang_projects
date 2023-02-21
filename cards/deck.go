3af1af0e3471382a6bb01cd345a8fe524e196320
Disha Dudhal <dishadudhal@Dishas-MacBook-Pro.local>
2023-02-21 04:05:14 -0500

    Convert deck to string, save deck to File and read Deck from file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 // Create a new type(data-structure) of deck
 // which is a slice of strings
@@ -14,7 +19,7 @@ func newDeck() deck {
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value + " of " + suit)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 
@@ -27,7 +32,33 @@ func (d deck) print() {
 	}
 }
 
-//function deal will return 2 values both of type deck
+// function deal will return 2 values both of type deck
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+// function to convert string to byte string for easy writing to a file
+func (d deck) toString() string {
+	return strings.Join([]string(d), "\n")
+}
+
+// d deck is a receiver, 0666 is file permissions viz linux, filename is param to func, error is return object
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func readDeckFromFile(filename string) deck {
+	byteSlice, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Option #1 - log the error and return a call to newDeck
+		// Option #2 - Log the error and entirely quit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	//if we were successful convert the byteSlice into string, then convert this string into
+	// slice of strings using inbuilt function Split.
+	sSlice := strings.Split(string(byteSlice), "\n")
+
+	//Convert the stringSlice into type deck and return
+	return deck(sSlice)
 }
--- cards/main.go
+++ cards/main.go
@@ -2,10 +2,11 @@ package main
 
 func main() {
 	// var card string = "Ace of Spades"   //You are telling Go compiler whats the type
-	cards := newDeck()
+	//cards := newDeck()
 	//cards.print()
-	hand, _ := deal(cards, 5)
-	hand.print()
-	//remainingCards.print()
-	cards.print()
+	//hand, _ := deal(cards, 5)
+	// fmt.Println(cards.toString())
+	//cards.saveToFile("my_cards")
+	newcards := readDeckFromFile("my_cards1")
+	newcards.print()
 }

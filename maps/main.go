33abde3034c36e7083989f4976e02d99e039898e
Disha Dudhal <dishadudhal@Dishas-MacBook-Pro.local>
2023-06-04 19:53:15 -0400

    How to add, delete and iterate over a Map in Go

--- maps/main.go
+++ maps/main.go
@@ -3,15 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	//Ways of declaring a map
-	var colors1 map[string]string
-
-	colors2 := make(map[string]string)
+	//Adding and deleting from a Map
 
 	colors := map[string]string{
-		"red":  "#ff0000",
-		"blue": "#123345",
+		"red":   "#ff0000",
+		"blue":  "#123345",
+		"green": "#098098",
+		"black": "#676576",
+	}
+	//Add an element to map
+	colors["white"] = "#345345"
+
+	//Delete an element from map
+	delete(colors, "blue")
+
+	//Iterate over the map
+	for key, val := range colors {
+		fmt.Println("Hex code for color", key, "is :", val)
 	}
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
 }
